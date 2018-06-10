6b301c4bb342dc95c06b81d5a1131fbcca7b4d0f
Elena Markova <webkwondo.com@gmail.com>
2018-06-10 09:51:22 +0300

    Правки по заданию #10

--- js/stat.js
+++ js/stat.js
@@ -5,15 +5,15 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var BLEED = 20;
+var CLOUD_PADDING = 20;
 var BAR_GAP = 50;
 var FONT_GAP = 16;
 var CHART_HEIGHT = 150;
 var CHART_INDENT = 25;
 var BAR_WIDTH = 40;
 var MAX_BAR_HEIGHT = CHART_HEIGHT - GAP;
-var chartX = CLOUD_X + BLEED;
-var legendY = CLOUD_Y + BLEED + FONT_GAP;
+var chartX = CLOUD_X + CLOUD_PADDING;
+var legendY = CLOUD_Y + CLOUD_PADDING + FONT_GAP;
 var legendHeight = legendY + FONT_GAP;
 
 var renderCloud = function (ctx, x, y, color) {
@@ -35,14 +35,36 @@ var renderCloud = function (ctx, x, y, color) {
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
-
-  for (var i = 0; typeof arr !== 'undefined' && arr.length > 0 && i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+  if (typeof arr !== 'undefined' && arr.length > 0) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
     }
   }
 
   return maxElement;
+};
+
+var getRandomColor = function () {
+  var randomSaturation = Math.round(Math.random() * (100 - 40) + 40);
+  var color = 'hsl(241, ' + randomSaturation + '%, 50%)';
+  return color;
+};
+
+var drawTimes = function (ctx, barTimesText, barTimesX, barTimesY, barTimesColor) {
+  ctx.fillStyle = barTimesColor;
+  ctx.fillText(barTimesText, barTimesX, barTimesY);
+};
+
+var drawBar = function (ctx, barX, barY, barWidth, barHeight, barColor) {
+  ctx.fillStyle = barColor;
+  ctx.fillRect(barX, barY, barWidth, barHeight);
+};
+
+var drawPlayers = function (ctx, barPlayersText, barPlayersX, barPlayersY, barPlayersColor) {
+  ctx.fillStyle = barPlayersColor;
+  ctx.fillText(barPlayersText, barPlayersX, barPlayersY);
 };
 
 window.renderStatistics = function (ctx, players, times) {
@@ -59,14 +81,23 @@ window.renderStatistics = function (ctx, players, times) {
   for (var i = 0; i < players.length; i++) {
     var k = times[i] / maxTime;
     var barHeight = MAX_BAR_HEIGHT * k;
-    var randomSaturation = Math.round(Math.random() * (100 - 40) + 40);
-    ctx.fillText(Math.round(times[i]), chartX + CHART_INDENT + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + BLEED + legendHeight + MAX_BAR_HEIGHT * (1 - k));
-    ctx.fillText(players[i], chartX + CHART_INDENT + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + BLEED + legendHeight + GAP + MAX_BAR_HEIGHT + FONT_GAP);
-    ctx.fillStyle = 'hsl(241, ' + randomSaturation + '%, 50%)';
+    var barTimesX = chartX + CHART_INDENT + (BAR_GAP + BAR_WIDTH) * i;
+    var barTimesY = CLOUD_Y + CLOUD_PADDING + legendHeight + MAX_BAR_HEIGHT * (1 - k);
+    var barPlayersX = chartX + CHART_INDENT + (BAR_GAP + BAR_WIDTH) * i;
+    var barPlayersY = CLOUD_Y + CLOUD_PADDING + legendHeight + GAP + MAX_BAR_HEIGHT + FONT_GAP;
+    var barX = chartX + CHART_INDENT + (BAR_GAP + BAR_WIDTH) * i;
+    var barY = CLOUD_Y + CLOUD_PADDING + legendHeight + GAP + MAX_BAR_HEIGHT * (1 - k);
+    var randomColor = getRandomColor();
+    var barTimesColor = '#000';
+    var barColor = randomColor;
+    var barPlayersColor = '#000';
+
+    drawTimes(ctx, Math.round(times[i]), barTimesX, barTimesY, barTimesColor);
     if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      barColor = 'rgba(255, 0, 0, 1)';
     }
-    ctx.fillRect(chartX + CHART_INDENT + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + BLEED + legendHeight + GAP + MAX_BAR_HEIGHT * (1 - k), BAR_WIDTH, barHeight);
-    ctx.fillStyle = '#000';
+    drawBar(ctx, barX, barY, BAR_WIDTH, barHeight, barColor);
+    drawPlayers(ctx, players[i], barPlayersX, barPlayersY, barPlayersColor);
   }
+
 };
