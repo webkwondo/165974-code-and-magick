d432a05485627f2c6fc92fb762574068ea3cd088
Elena Markova <webkwondo.com@gmail.com>
2018-07-18 00:46:55 +0300

    Перетаскивание диалога редактирования персонажа. Работы по заданию #17

--- js/setup.js
+++ js/setup.js
@@ -104,6 +104,12 @@ var openPopup = function () {
 
 var closePopup = function () {
   setup.classList.add('hidden');
+  if (setup.style.top) {
+    setup.style.removeProperty('top');
+  }
+  if (setup.style.left) {
+    setup.style.removeProperty('left');
+  }
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
@@ -191,4 +197,36 @@ wizardEyes.addEventListener('click', function () {
 
 wizardFireball.addEventListener('click', function () {
   changeColor(fireballColorArr, wizardFireball, wizardFireballInput, true);
+});
+
+var shopElement = document.querySelector('.setup-artifacts-shop');
+var draggedItem = null;
+
+shopElement.addEventListener('dragstart', function (evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plain', evt.target.alt);
+  }
+});
+
+var artifactsElement = document.querySelector('.setup-artifacts');
+
+artifactsElement.addEventListener('dragover', function (evt) {
+  evt.preventDefault();
+  return false;
+});
+
+artifactsElement.addEventListener('drop', function (evt) {
+  evt.target.style.backgroundColor = '';
+  evt.target.appendChild(draggedItem);
+});
+
+artifactsElement.addEventListener('dragenter', function (evt) {
+  evt.target.style.backgroundColor = 'yellow';
+  evt.preventDefault();
+});
+
+artifactsElement.addEventListener('dragleave', function (evt) {
+  evt.target.style.backgroundColor = '';
+  evt.preventDefault();
 });
